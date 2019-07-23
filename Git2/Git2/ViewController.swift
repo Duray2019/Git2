c9dc8eae315dd0e6dd75b48576cb2f71633387f5
Durjay Singha <durjay.singha@webspiders.com>
2019-07-23 18:51:56 +0530

    testing

--- Git2/Git2/ViewController.swift
+++ Git2/Git2/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Xcode 11")
+         print("Xcode 11")
         // Do any additional setup after loading the view.
     }
 
